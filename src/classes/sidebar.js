3efba3c6e50f3c2d38fb7426ebbe3c9b9c62799a
xdpiqbx <xdpiqbx@gmail.com>
2021-08-10 08:30:32 +0300

    pitstop =)

--- src/classes/sidebar.js
+++ src/classes/sidebar.js
@@ -8,14 +8,22 @@ export class Sidebar {
 
   init() {
     this.$el.insertAdjacentHTML('afterbegin', this.template);
-    this.$el.addEventListener('submit', this.add);
+    this.$el.addEventListener('submit', this.addHandler);
   }
 
   get template() {
     return [block('Title'), block('Text')].join('');
   }
 
-  add(event) {
+  addHandler(event) {
     event.preventDefault();
+    // console.log(event.target); // Вся форма
+    // console.log(event.target.value.value); //значение input name="value"
+    // console.log(event.target.style.value); //значение input name="style"
+    const type = event.target.name;
+    const value = event.target.value.value;
+    const style = event.target.style.value;
+    debugger;
+    //https://youtu.be/0ViiJ8qTCFM?t=5997
   }
 }
